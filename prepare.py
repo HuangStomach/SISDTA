6da6530d901399cff2e7f475c9f9069264984641
HuangStomach <nxmbest@qq.com>
2022-02-23 19:36:57 +0800

    删除fcfps功能 加入go抓取

--- prepare.py
+++ prepare.py
@@ -2,6 +2,8 @@ import requests
 import json
 import pandas as pd
 import numpy as np
+from time import sleep
+from urllib import request
 from transformers import BertModel, BertTokenizer
 from rdkit import Chem
 from rdkit.Chem import AllChem
@@ -67,6 +69,34 @@ def protein_token():
             gc.collect()
             print(protein, 'OK')
 
+def protein_go():
+    seqs = []
+    protein_dict = np.loadtxt('./data/protein/proteins.csv', dtype=str, delimiter=',')[:, 0]
+    protein_url = 'https://rest.uniprot.org/beta/uniprotkb/{}.json'
+
+    for protein in protein_dict:
+        sleep(1)
+        try:
+            req = request.Request(protein_url.format(protein), headers={
+                'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36'
+            })
+            data = request.urlopen(req).read()
+            text = json.loads(data)
+            
+            go_vectos = []
+            for item in text['uniProtKBCrossReferences']:
+                if item['database'] != 'GO': continue
+                go_vectos.append(item['id'])
+
+            seqs.append([protein, ";".join(go_vectos)])
+            print(protein, 'OK')
+
+        except Exception as e:
+            print(protein, e)
+            seqs.append([protein, 'ERROR'])
+    
+    np.savetxt('./data/protein/protein_go.csv', seqs, fmt='%s', delimiter=',')
+
 def drug_smile():
     seqs = []
 
@@ -96,38 +126,9 @@ def drug_ecfps():
 
     np.savetxt('./data/drug/ecfps.csv', seqs, fmt='%s', delimiter=',')
 
-def drug_fcfps():
-    seqs = []
-
-    drugs = np.loadtxt('./data/drug/smiles.csv', delimiter=',', dtype=str, comments=None)
-    for drug in drugs:
-        try:
-            name, smiles = drug
-            mol = Chem.MolFromSmiles(smiles)
-            seqs.append(AllChem.GetMorganFingerprintAsBitVect(mol, 3, nBits=1024, useFeatures=True).ToList())
-            # fp1 = AllChem.GetMorganFingerprint(mol, 3)
-            # print(fp1.GetNonzeroElements())
-            quit()
-        except Exception as e:
-            print(drug)
-
-    np.savetxt('./data/drug/fcfps.csv', seqs, fmt='%s', delimiter=',')
-
-def drug_sim():
-    seqs = []
-    
-    drugs = np.loadtxt('./data/drug/smiles.csv', delimiter=',', dtype=str, comments=None)
-    for name_A, smiles_A in drugs:
-        for name_B, smiles_B in drugs:
-            print(jaccard_score(list(smiles_A), list(smiles_B), average='macro'))
-        quit()   
-
-    # np.savetxt('./data/drug/fcfps.csv', seqs, fmt='%s', delimiter=',')
-
 if __name__=='__main__':
     # protein()
     # protein_token()
     # drug_smile()
     # drug_ecfps()
-    # drug_fcfps()
-    drug_sim()
+    protein_go()
