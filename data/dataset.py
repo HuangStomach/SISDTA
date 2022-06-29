31937936be4a963a83b5e4533da11eaf6e53e5dd
HuangStomach <nxmbest@qq.com>
2022-06-29 16:58:19 +0800

    数据集合checkexist

--- data/dataset.py
+++ data/dataset.py
@@ -1,6 +1,13 @@
+import os
+import json
 import numpy as np
+
 import torch
 from torch.utils.data import Dataset
+import deepchem as dc
+from rdkit import Chem
+from rdkit.Chem import AllChem
+
 from .kiba.kiba import Kiba
 
 handlers = {
@@ -13,9 +20,12 @@ class MultiDataset(Dataset):
         self.device = device
         self.train = train
         self.handler = handlers[type](self.train)
+        self._check_exists()
+        self.handler._load_data(train)
 
         self.d_vecs = torch.tensor(self.handler.d_vecs, dtype=torch.float32, device=self.device)
         self.d_ecfps = torch.tensor(self.handler.d_ecfps, dtype=torch.float32, device=self.device)
+        self.d_intersect = self.handler.d_intersect
         self.p_embeddings = torch.tensor(self.handler.p_embeddings, dtype=torch.float32, device=self.device)
         self.p_gos = torch.tensor(self.handler.p_gos, dtype=torch.float32, device=self.device)
         y = self.handler.y
@@ -32,13 +42,37 @@ class MultiDataset(Dataset):
             indexes.append([i, j])
             targets.append(y[i][j])
 
+
         if self.train:
+            # 对比学习所需的分类
             for v in targets:
                 classes.append(int(v / unit))
             self.classes = torch.tensor(classes, dtype=torch.long, device=self.device)
+            
+            # 根据训练集对d_intersect矩阵进行mask
+            train_d = np.unique(drugs)
+            mask = np.ones(self.d_intersect.shape[0], bool)
+            mask[train_d] = False
+            self.d_intersect[mask] = 0.0
+            self.d_intersect[:, mask] = 0.0
 
         self.indexes = torch.tensor(indexes, dtype=torch.long, device=self.device)
         self.targets = torch.tensor(targets, dtype=torch.float32, device=self.device).view(-1, 1)
+
+    def _graph_gen(self, feature, matrix):
+        for i in range(len(feature)):
+            edge_index = []
+            x = [feature[i]]
+            neighbors = (-matrix[i]).argsort()[1:6]
+            for j in neighbors:
+                edge_index.append([0, j + 1])
+                edge_index.append([j + 1, 0])
+                x.append(feature[j])
+
+            edge_index = torch.tensor(edge_index, dtype=torch.long, device=self.device)
+            x = torch.tensor(x, dtype=torch.float32, device=self.device)
+            data = Data(x=x, edge_index=edge_index.t().contiguous())
+        pass
 
     def __getitem__(self, index):
         dindex, pindex = self.indexes[index]
@@ -54,4 +88,41 @@ class MultiDataset(Dataset):
         return self.indexes.size(dim=0)
 
     def _check_exists(self):
-        self.handler._check_exists()
+        print('checking data file exists...')
+        if not os.path.exists(self.handler.d_ecfps_path):
+            print('generating drug ecfps...')
+            radius = 4
+            seqs = []
+            with open(self.handler.ligands_path) as fp:
+                drugs = json.load(fp)
+
+                for drug in drugs:
+                    try:
+                        smiles = drugs[drug]
+                        mol = Chem.MolFromSmiles(smiles)
+                        seqs.append(AllChem.GetMorganFingerprintAsBitVect(mol, radius, nBits=1024).ToList())
+                    except Exception as e:
+                        print(drug)
+            np.savetxt(self.handler.d_ecfps_path, seqs, fmt='%d', delimiter=',')
+
+        if not os.path.exists(self.handler.d_vecs_path):
+            print('generating drug vectors...')
+            with open(self.handler.ligands_path) as fp:
+                drugs = json.load(fp)
+            smiles = [drugs[drug] for drug in drugs]
+            featurizer = dc.feat.Mol2VecFingerprint()
+            features = featurizer.featurize(smiles)
+
+            np.savetxt(self.handler.d_vecs_path, features, fmt='%s', delimiter=',')
+        
+        if not os.path.exists(self.handler.d_intersect_path):
+            print('generating drug intersect...')
+            drug_ecfps = np.loadtxt(self.handler.d_ecfps_path, delimiter=',', dtype=int, comments=None)
+            drug_count = drug_ecfps.shape[0]
+            matrix = np.zeros((drug_count, drug_count))
+
+            for i in range(drug_count):
+                for j in range(drug_count):
+                    inter = np.sum(np.bitwise_and(drug_ecfps[i], drug_ecfps[j]))
+                    matrix[i][j] = round(1 - ((np.sum(drug_ecfps[j]) - inter) / np.sum(drug_ecfps[j])), 6)
+            np.savetxt(self.handler.d_intersect_path, matrix, fmt='%s', delimiter=',')
--- data/kiba/kiba.py
+++ data/kiba/kiba.py
@@ -1,16 +1,18 @@
-import os
 import pandas as pd
 import numpy as np
 import json
-import deepchem as dc
-from rdkit import Chem
-from rdkit.Chem import AllChem
 
 class Kiba:
     def __init__(self, train = True):
-        self._check_exists()
-        self.d_vecs = np.loadtxt('./data/kiba/drug_vec.csv', delimiter=',', dtype=float, comments=None)
-        self.d_ecfps = np.loadtxt('./data/kiba/drug_ecfps.csv', delimiter=',', dtype=int, comments=None)
+        self.ligands_path = './data/kiba/ligands_can.json'
+        self.d_ecfps_path = './data/kiba/drug_ecfps.csv'
+        self.d_vecs_path = './data/kiba/drug_vec.csv'
+        self.d_intersect_path = './data/kiba/drug_intersect.csv'
+
+    def _load_data(self, train = True):
+        self.d_vecs = np.loadtxt(self.d_vecs_path, delimiter=',', dtype=float, comments=None)
+        self.d_ecfps = np.loadtxt(self.d_ecfps_path, delimiter=',', dtype=int, comments=None)
+        self.d_intersect = np.loadtxt(self.d_intersect_path, delimiter=',', dtype=float, comments=None)
         self.p_embeddings = pd.read_csv('./data/kiba/protein_embedding.csv', delimiter=',', header=None, index_col=0).to_numpy(float)
         self.p_gos = pd.read_csv('./data/kiba/protein_go_vector.csv', delimiter=',', header=0, index_col=0).to_numpy(float)
         self.y = np.loadtxt('./data/kiba/Y.txt', delimiter=',', dtype=float, comments=None)
@@ -24,29 +26,3 @@ class Kiba:
 
         rows, cols = np.where(np.isnan(self.y) == False)
         self.drugs, self.proteins = rows[indexes], cols[indexes]
-
-    def _check_exists(self):
-        if not os.path.exists('./data/kiba/drug_ecfps.csv'):
-            radius = 4
-            seqs = []
-            with open("./data/kiba/ligands_can.json") as fp:
-                drugs = json.load(fp)
-
-                for drug in drugs:
-                    try:
-                        smiles = drugs[drug]
-                        mol = Chem.MolFromSmiles(smiles)
-                        seqs.append(AllChem.GetMorganFingerprintAsBitVect(mol, radius, nBits=1024).ToList())
-                    except Exception as e:
-                        print(drug)
-            np.savetxt('./data/kiba/drug_ecfps.csv', seqs, fmt='%d', delimiter=',')
-
-        if not os.path.exists('./data/kiba/drug_vec.csv'):
-            print('generating drug vectors...')
-            with open("./data/kiba/ligands_can.json") as fp:
-                drugs = json.load(fp)
-            smiles = [drugs[drug] for drug in drugs]
-            featurizer = dc.feat.Mol2VecFingerprint()
-            features = featurizer.featurize(smiles)
-
-            np.savetxt('./data/kiba/drug_vec.csv', features, fmt='%s', delimiter=',')
