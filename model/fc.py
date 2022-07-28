8af703675e80548f1817066f9edf3907fda210ef
HuangStomach <nxmbest@qq.com>
2022-07-28 19:14:02 +0800

    删除掉dropout

--- model/fc.py
+++ model/fc.py
@@ -25,28 +25,23 @@ class FC(nn.Module):
         )
 
         self.output = nn.Sequential(
-            nn.Linear(256, 128),
-            nn.BatchNorm1d(128),
-            nn.Dropout(0.2),
+            nn.Linear(256, 64),
+            nn.BatchNorm1d(64),
             nn.LeakyReLU(),
-            nn.Linear(128, 1),
+            nn.Linear(64, 1),
         )
 
         self.d_gcn = Sequential('x, edge_index, edge_weight', [
             (GCNConv(1024, 1024), 'x, edge_index, edge_weight -> x1'),
-            nn.Dropout(0.4),
             nn.LeakyReLU(),
         ])
 
         self.p_gcn = Sequential('x, edge_index, edge_weight', [
             (GCNConv(2812, 1024), 'x, edge_index, edge_weight -> x1'),
-            nn.Dropout(0.4),
             nn.LeakyReLU(),
         ])
 
     def forward(self, d_index, p_index, d_vecs, p_embeddings, y, dataset):
-        feature = torch.cat((d_vecs, p_embeddings), dim = 1)
-
         ecfps = self.d_gcn(dataset.d_ecfps, dataset.d_edge_index, dataset.d_edge_weight)[d_index]
         gos = self.p_gcn(dataset.p_gos, dataset.p_edge_index, dataset.p_edge_weight)[p_index]
 
