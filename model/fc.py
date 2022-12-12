934400c58f7a64144e516d1c5d63c862408f106c
HuangStomach <nxmbest@qq.com>
2022-12-12 11:00:36 +0800

    删除冗余注释

--- model/fc.py
+++ model/fc.py
@@ -5,19 +5,7 @@ from torch_geometric.nn import Sequential, GCNConv
 class FC(nn.Module):
     def __init__(self, p_gos_dim):
         super(FC, self).__init__()
-        # vector embedding ecfps gos ecfps gos
-        # self.dsim = dsim
-        # self.dcsi = dcsi
-        # self.psim = psim
-        # self.pcsi = pcsi
-
         dim = 300 + 1024 + 1024 + 1024;
-        # if self.dsim: dim += 1024
-        # if self.dcsi: dim += 1024
-        # if self.psim: dim += 1024
-        # if self.pcsi: dim += 1024
-        # if not self.dcsi and not self.pcsi and not self.dsim and not self.psim:
-        #     dim += 1024 + p_gos_dim
 
         self.encoder = nn.Sequential(
             nn.Linear(dim, 2048),
@@ -44,15 +32,6 @@ class FC(nn.Module):
             nn.Linear(256, 1),
         )
 
-        # if self.dcsi: self.ecfps_csi = Sequential('x, edge_index, edge_weight', [
-        #     (GCNConv(1024, 1024), 'x, edge_index, edge_weight -> x1'),
-        #     nn.LeakyReLU(),
-        # ])
-        # if self.pcsi: self.gos_csi = Sequential('x, edge_index, edge_weight', [
-        #     (GCNConv(p_gos_dim, 1024), 'x, edge_index, edge_weight -> x1'),
-        #     nn.LeakyReLU(),
-        # ])
-
         self.ecfps_sim = Sequential('x, edge_index, edge_weight', [
             (GCNConv(1024, 1024), 'x, edge_index, edge_weight -> x1'),
             nn.LeakyReLU(),
@@ -63,20 +42,8 @@ class FC(nn.Module):
         ])
 
     def forward(self, d_index, p_index, d_vecs, p_embeddings, dataset):
-        # features = [d_vecs, p_embeddings]
-        
-        # if self.dcsi:
-        #     features.append(self.ecfps_csi(dataset.d_ecfps, dataset.d_inter_ei, dataset.d_inter_ew)[d_index])
-        # if self.pcsi:
-        #     features.append(self.gos_csi(dataset.p_gos, dataset.p_inter_ei, dataset.p_inter_ew)[p_index])
-
-        # if self.dsim:
         ecfps = self.ecfps_sim(dataset.d_ecfps, dataset.d_sim_ei, dataset.d_sim_ew)[d_index]
         gos = self.gos_sim(dataset.p_gos, dataset.p_sim_ei, dataset.p_sim_ew)[p_index]
-        # if self.psim:
-        # if not self.dcsi and not self.pcsi and not self.dsim and not self.psim:
-        #     features.append(dataset.d_ecfps[d_index])
-        #     features.append(dataset.p_gos[p_index])
 
         feature = torch.cat((d_vecs, p_embeddings, ecfps, gos), dim = 1)
         encoded = self.encoder(feature)
